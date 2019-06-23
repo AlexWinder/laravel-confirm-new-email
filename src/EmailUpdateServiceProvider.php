7019321e2156f040799edd5e2c0cc1d12e96f407
Alex Winder <contact@alexwinder.uk>
2019-06-23 21:26:18 +0100

    php artisan make:provider ConfirmNewEmailUpdateServiceProvider

---
+++ src/EmailUpdateServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AlexWinder\ConfirmNewEmail;
+
+use Illuminate\Support\ServiceProvider;
+
+class ConfirmNewEmailServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
