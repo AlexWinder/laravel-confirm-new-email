76c8ba692a4ffe3e9cdf07cf970e843023ed12e5
Alex Winder <contact@alexwinder.uk>
2019-06-25 21:36:01 +0100

    Use the web and auth middleware on the existing routes

--- src/routes.php
+++ src/routes.php
@@ -2,8 +2,10 @@
 
 // Route which provides user a form to update their email address
 Route::get(config('confirm-new-email.route.edit.uri'), '\AlexWinder\ConfirmNewEmail\ConfirmNewEmailController@edit')
-    ->name(config('confirm-new-email.route.edit.name'));
+    ->name(config('confirm-new-email.route.edit.name'))
+    ->middleware(['web', 'auth']);
 
 // Route which is used to update a users email address
 Route::post(config('confirm-new-email.route.update.uri'), '\AlexWinder\ConfirmNewEmail\ConfirmNewEmailController@update')
-    ->name(config('confirm-new-email.route.update.name'));
+    ->name(config('confirm-new-email.route.update.name'))
+    ->middleware(['web', 'auth']);
