1dd34c9f09f583e0a8114f138513d6755142c54f
Alex Winder <contact@alexwinder.uk>
2019-06-25 20:34:29 +0100

    php artisan make:controller ConfirmNewEmailController

---
+++ src/ConfirmNewEmailController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AlexWinder\ConfirmNewEmail;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ConfirmNewEmailController extends Controller
+{
+    //
+}
