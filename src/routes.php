533cd5ecb38f98a4970dc61a2975fca7f576edbf
Alex Winder <contact@alexwinder.uk>
2019-06-25 21:20:05 +0100

    Added the edit GET route

--- src/routes.php
+++ src/routes.php
@@ -1,2 +1,5 @@
 <?php
 
+// Route which provides user a form to update their email address
+Route::get(config('confirm-new-email.route.edit.uri'), '\AlexWinder\ConfirmNewEmail\ConfirmNewEmailController@edit')
+    ->name(config('confirm-new-email.route.edit.name'));
