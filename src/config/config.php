e3493a948529edcf723050012de18204f4cea8ba
Alex Winder <contact@alexwinder.uk>
2019-06-29 22:03:33 +0100

    Updated route names and URIs in config

--- src/config/config.php
+++ src/config/config.php
@@ -11,7 +11,8 @@ return [
     | from inside your application, or if you wish to change the name for
     | cosmetic purposes. For convenience a route name has also been defined
     | and should also be updated if this is likely to conflict with other
-    | route names inside your application.
+    | route names inside your application. It is important that all values
+    | set in these routes should all contain unique 'name' values.
     |
     */
     'route' => [
@@ -24,11 +25,19 @@ return [
             'name' => 'confirm-new-email.edit',
         ],
         /**
-         * The update route is that which is used to update the users email address.
+         * The update-request route is that which is used to request an update to the users email address.
          */
-        'update' => [
+        'update-request' => [
             'uri' => '/settings/email/edit',
-            'name' => 'confirm-new-email.update',
+            'name' => 'confirm-new-email.update-request',
+        ],
+        /**
+         * The update-confirm route is that which is used to confirm the update to the users email address.
+         * The URI value set here should not match the URI value of any other route.
+         */
+        'update-confirm' => [
+            'uri' => '/settings/email/update',
+            'name' => 'confirm-new-email.update-confirm',
         ],
     ],
 ];
