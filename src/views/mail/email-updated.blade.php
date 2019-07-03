10ad8204d33ed68be66cf6e64ccd0bb7a8099aa0
Alex Winder <contact@alexwinder.uk>
2019-07-03 21:18:16 +0100

    Changed from email to e-mail

--- src/views/mail/email-updated.blade.php
+++ src/views/mail/email-updated.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # E-Mail Address Updated
 
-You are receiving this email as you have updated the email address on your account from {{ $old_email }} to {{ $new_email }}. 
+You are receiving this e-mail as you have updated the e-mail address on your account from {{ $old_email }} to {{ $new_email }}. 
 
 If you did not request this action then please contact a system administrator immediately.
 
